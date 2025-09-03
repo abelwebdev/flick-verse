ef4b5dcb5782c6b02cf2d1505b45469662c0834a
abelwebdev <82320458+abelwebdev@users.noreply.github.com>
2025-09-03 18:13:33 +0300

    added new streaming source

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -10,11 +10,12 @@
   }
 
   export const getMovieServerUrls = (id: number | string): MovieServerUrlItem[] => [
-    { key: "vidsrc", name: "Server 1", url: `https://vidsrc.icu/embed/movie/${id}?autoplay=1&muted=1` },
-    { key: "2embed", name: "Server 2", url: `https://2embed.cc/embed/${id}` },
-    { key: "multiembed", name: "Server 3", url: `https://multiembed.mov/?video_id=${id}&tmdb=1` },
-    { key: "moviesapi", name: "Server 4", url: `https://moviesapi.club/movie/${id}` },
-    { key: "autoembed", name: "Server 5", url: `https://autoembed.co/movie/tmdb/${id}` },
+    { key: "vidlink", name: "Server 1", url: `https://vidlink.pro/movie/${id}?autoplay=false` },
+    { key: "vidsrc", name: "Server 2", url: `https://vidsrc.icu/embed/movie/${id}?autoplay=1&muted=1` },
+    { key: "2embed", name: "Server 3", url: `https://2embed.cc/embed/${id}` },
+    { key: "multiembed", name: "Server 4", url: `https://multiembed.mov/?video_id=${id}&tmdb=1` },
+    { key: "moviesapi", name: "Server 5", url: `https://moviesapi.club/movie/${id}` },
+    { key: "autoembed", name: "Server 6", url: `https://autoembed.co/movie/tmdb/${id}` },
   ];
 
   export const getTvServerUrls = (
@@ -23,28 +24,33 @@
     episode: number | string
   ): TvServerUrlItem[] => [
     {
-      key: "vidsrc",
+      key: "vidlink",
       name: "Server 1",
+      url: `https://vidlink.pro/tv/${id}/${season}/${episode}?autoplay=false`,
+    },
+    {
+      key: "vidsrc",
+      name: "Server 2",
       url: `https://vidsrc.icu/embed/tv/${id}/${season}/${episode}?autoplay=1&muted=1`,
     },
     {
       key: "2embed",
-      name: "Server 2",
+      name: "Server 3",
       url: `https://2embed.cc/embedtv/${id}?s=${season}&e=${episode}`,
     },
     {
       key: "multiembed",
-      name: "Server 3",
+      name: "Server 4",
       url: `https://multiembed.mov/?tmdb=1&video_id=${id}&s=${season}&e=${episode}`,
     },
     {
       key: "moviesapi",
-      name: "Server 4",
+      name: "Server 5",
       url: `https://moviesapi.club/tv/${id}-${season}-${episode}`,
     },
     {
       key: "autoembed",
-      name: "Server 5",
+      name: "Server 6",
       url: `https://autoembed.co/tv/tmdb/${id}-${season}-${episode}`,
     },
   ];
